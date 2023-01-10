3109f8a04bf9b910453d27c2769e74a015db0d61
anas jabellah <jablcodiing@gmail.com>
2023-01-10 11:18:07 +0100

    add  methed claims token

--- config/JwtService.java
+++ config/JwtService.java
@@ -1,10 +1,25 @@
 package com.example.bookingmt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
         return  null ;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInkey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private byte[] getSignInkey() {
+        return null ;
     }
 }
